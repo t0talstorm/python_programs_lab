4e3d9aea855c5bd806211e4e26c53a70c2cacb9a
Aditya Sinha <adityaishan007@gmail.com>
2025-04-28 10:18:19 +0530

    expt_4

---
+++ Set_operations.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print("The sum of the two numbers is : ", a + b)
+print("The Substraction of the two numbers is : ", a - b)
+print("The multiplication of the two numbers is : ", a * b)
+
+if a == 0:
+    print("Num 2 cannot be zero")
+else:
+    print("The division of the two numbers is : ", a / b)
+    
+        
